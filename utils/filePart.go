705f01fac583d419e32b5847aaf9229e92dff343
xuhe <xuhe114514@gmail.com>
2024-08-09 19:39:18 +0800

    feat: Download func

--- utils/filePart.go
+++ utils/filePart.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
 type FilePart struct {
@@ -14,10 +15,11 @@ type FilePart struct {
 	Data  []byte
 	// signal channel for download finish
 	// 0 is success, 1 is failed
-	finishSignal chan int
+	processSignal chan int
 }
 
-func (fp *FilePart) Download() {
+func (fp *FilePart) Download(wg *sync.WaitGroup) {
+	defer wg.Done()
 	// craete header for http request
 	header := map[string]string{
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64",
@@ -26,34 +28,29 @@ func (fp *FilePart) Download() {
 	// craete a new request
 	req := NewHTTPRequest(fp.Url, header)
 	if req == nil {
-		fp.finishSignal <- 1 // download failed
 		return
 	}
 	// start download
 	log.Printf("Start download part %d", fp.Index)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Printf("Error downloading part %d: %v", fp.Index, err)
-		fp.finishSignal <- 1 // download failed
+		log.Print(ColorString(fmt.Sprintf("Error downloading part %d: %v", fp.Index, err), Red))
 		return
 	}
 	defer resp.Body.Close()
 	// read response body
 	size, err := resp.Body.Read(fp.Data)
 	if err != nil {
-		log.Printf("Error reading part %d: %v", fp.Index, err)
-		fp.finishSignal <- 1 // download failed
+		log.Print(ColorString(fmt.Sprintf("Error downloading part %d: %v", fp.Index, err), Red))
 		return
 	}
 	// check response size
 	if int64(size) != fp.To-fp.From+1 {
-		log.Printf("Error downloading part %d: expected %d bytes, got %d bytes", fp.Index, fp.To-fp.From+1, size)
-		fp.finishSignal <- 1 // download failed
+		log.Print(ColorString(fmt.Sprintf("Error downloading part %d: expected %d bytes, got %d bytes", fp.Index, fp.To-fp.From+1, size), Red))
 		return
 	}
-	fp.finishSignal <- 0 // download success
 }
 
-func (fp *FilePart) GetFinishSignal() <-chan int {
-	return fp.finishSignal
+func (fp *FilePart) GetProcess() int {
+	return <-fp.processSignal
 }
